d9e66538ddb592e7bb52a6307b6f4557d001762b
filippe24 <jovaealberto@hotmail.com>
2019-06-24 23:39:24 +0200

    first commit

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+
+    Waypoint waypoint;
+
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+
+
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x,
+            0f,
+            waypoint.GetGridPos().y
+        );
+    }
+
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        int gridSize = waypoint.GetGridSize();
+        string labelText = 
+            waypoint.GetGridPos().x / gridSize +
+            "," + 
+            waypoint.GetGridPos().y / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
